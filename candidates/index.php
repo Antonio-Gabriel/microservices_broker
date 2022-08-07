8bf85c5f116fe3fd74f67c1d2061f71290305d71
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-08-07 03:37:11 +0100

    feat: add docker file and docker-compose, configure nginx server

--- candidates/index.php
+++ candidates/index.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once "./vendor/autoload.php";
-require_once "./scripts/config.php";
+require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/scripts/config.php";
 
-$http = require_once "./src/infra/http/http.php";
+$http = require_once __DIR__ . "/src/infra/http/http.php";
 
 use Slim\App;
 
@@ -19,6 +19,10 @@ $app->add(function ($req, $res, $next) {
         ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+$app->get("/", function ($req, $res) {
+    return $res->withJson(["msg" => "welcome to candidates service"]);
 });
 
 $http($app);
