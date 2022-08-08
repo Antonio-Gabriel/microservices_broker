697f042082db804f8319f2418018cfdea758d594
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-08-08 18:17:07 +0100

    feat: add vote route

--- candidates/src/infra/http/http.php
+++ candidates/src/infra/http/http.php
@@ -2,13 +2,20 @@
 
 use Slim\App;
 
-use Candidates\application\usecases\CreateCategory;
+use Candidates\infra\repositories\CandidatesRepository;
 use Candidates\infra\repositories\CategoriesRepository;
 
-return function (App $app) {
-    $app->post("/category", function ($req, $res) {
+use Candidates\application\usecases\ListCandidatesUseCase;
+use Candidates\application\usecases\ListCategoriesUseCase;
 
-        $createCategory = new CreateCategory(new CategoriesRepository);
+use Candidates\application\usecases\CreateCategoryUseCase;
+use Candidates\application\usecases\CreateCandidateUseCase;
+use Candidates\application\usecases\VoteUseCase;
+
+return function (App $app) {
+    $app->post("/categories", function ($req, $res) {
+
+        $createCategory = new CreateCategoryUseCase(new CategoriesRepository);
 
         try {
             $result = $createCategory->execute($req->getParsedBody()["name"]);
@@ -18,6 +25,49 @@ return function (App $app) {
             ]]);
         } catch (\Exception $e) {
             return $res->withStatus(400)->withJson(["error" => $e->getMessage()]);
-        }    
+        }
+    });
+
+    $app->get("/categories", function ($req, $res) {
+
+        $categories = new ListCategoriesUseCase(new CategoriesRepository);
+
+        return $res->withStatus(200)->withJson($categories->execute());
+    });
+
+    // Candidates routes
+    $app->post("/candidates", function ($req, $res) {
+
+        $createCandidate = new CreateCandidateUseCase(new CandidatesRepository);
+
+        try {
+            $result = $createCandidate->execute($req->getParsedBody()["name"], $req->getParsedBody()["categoryId"]);
+
+            return $res->withStatus(200)->withJson(["data" => [
+                "id" => $result
+            ]]);
+        } catch (\Exception $e) {
+            return $res->withStatus(400)->withJson(["error" => $e->getMessage()]);
+        }
+    });
+
+    $app->get("/candidates", function ($req, $res) {
+        $candidates = new ListCandidatesUseCase(new CandidatesRepository);
+
+        return $res->withStatus(200)->withJson($candidates->execute());
+    });
+
+    $app->post("/vote", function ($req, $res) {
+        $vote = new VoteUseCase(new CandidatesRepository());
+
+        try {
+            $vote->execute(
+                $req->getParsedBody()["candidateId"],
+                $req->getParsedBody()["voter"],
+                $req->getParsedBody()["voterEmail"]
+            );
+        } catch (\Exception $e) {
+            return $res->withStatus(400)->withJson(["error" => $e->getMessage()]);
+        }
     });
 };
