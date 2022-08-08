f4d178d19c056b45e52ced73286d5946ca5d3f64
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-08-08 18:18:39 +0100

    feat: create vote usecase and add kafka

---
+++ candidates/src/application/usecases/VoteUseCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Candidates\application\usecases;
+
+use Candidates\common\Log;
+use Candidates\domain\exceptions\CandidateDoNotExists;
+use Candidates\application\repositories\ICandidatesRepository;
+
+use Candidates\infra\messaging\kafka\KafkaProducer;
+
+use Exception;
+
+class VoteUseCase
+{
+    private KafkaProducer $producer;
+
+    public function __construct(
+        private ICandidatesRepository $candidatesRepository
+    ) {
+        $this->producer = new KafkaProducer();
+    }
+
+    public function execute(string $candidateId, string $voter, string $voterEmail)
+    {
+        $candidate = $this->candidatesRepository->findByCandidateId($candidateId);
+
+        if (!$candidate) {
+            Log::set("Candidate doesn't exists!", "error", [
+                "candidateId" => $candidateId
+            ]);
+
+            throw new CandidateDoNotExists("Candidate doesn't exists!");
+        }
+
+        if (empty($voter) || empty($voterEmail)) {
+            throw new Exception("Check the input data");
+        }
+
+        $this->producer->send([
+            'topic'     => 'vote.new_vote',
+            'value'     => [
+                'voter'             =>  $voter,
+                'voterEmail'        =>  $voterEmail,
+                'candidateId'       =>  $candidate->getId(),
+                'candidateName'     =>  $candidate->getName(),
+            ],
+            'key'       => 'vote.new_vote_key',
+        ]);
+    }
+}
